a8e9210e2502f72bc0fc580e928035f9d644cdf4
Yarukii734 <bojtipatrik01@gmail.com>
2025-02-12 17:15:47 +0100

    components

---
+++ resources/views/components/scripts.blade.php
@@ -0,0 +1,13 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="{{ asset('assets/notification.js') }}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script>
+    		function seenPassword() {
+			var x = document.getElementById("password");
+			if (x.type === "password") {
+				x.type = "text";
+			} else {
+				x.type = "password";
+			}
+		}
+</script>
