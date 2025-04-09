d58d0ad1d8072c74d5c7e9b677734541165b37bf
Yarukii734 <bojtipatrik01@gmail.com>
2025-04-09 20:50:44 +0200

    Update auth.blade.php

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,43 +1,82 @@
-<!DOCTYPE html>
-<html lang="hu">
+<html lang="en">
+    <head>
+    
     @include('components.header')
-	<body id="kt_body" class="bg-body" >
-		<div class="d-flex flex-column flex-root">
-			<div class="d-flex flex-column flex-lg-row flex-column-fluid">
-				<div class="d-flex flex-column flex-lg-row-auto w-xl-600px positon-xl-relative" style="background-image:url(https://i.redd.it/iuwm9wigep651.png);">
-					<div class="d-flex flex-column position-xl-fixed top-0 bottom-0 w-xl-600px scroll-y">
-						<div class="d-flex flex-row-fluid flex-column text-center p-10 pt-lg-20">
-							<center>
-								<div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-120px" style="background-image: url('https://i.imgur.com/G03MurG.png');height: 150px;width: 150px;">
-								</div>
-							</center>
-						</div>
-            		<div class="d-flex flex-row-fluid flex-column text-center p-10 pt-lg-20" style="@import 'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap';font-family:poppins,sans-serif!important;">
-						<div class="flex-column-fluid d-flex flex-column justify-content-center">
-							<p class="font-weight-lighter text-white opacity-80" style="@import 'https://fonts.googleapis.com/css?family=Poppins:400,700&amp;display=swap';font-family:poppins,sans-serif!important;"><u><b>ChromeCinema</b></u></p>
-								<p class="font-weight-lighter text-white opacity-80" style="@import 'https://fonts.googleapis.com/css?family=Poppins:400,700&display=swap';font-family:poppins,sans-serif!important;">Lépj be a mozi világába, vásárolj jegyet a neked tetsző filmre, mellé egy jó nassolnivalót, és élvezd a mozi világát.<br> Ha nincs még fiókod, kattints a regisztráció felületre!</p>
-						</div>
-					</div>
-						<div class="d-flex flex-row-auto bgi-no-repeat bgi-position-x-center bgi-size-contain bgi-position-y-bottom min-h-100px min-h-lg-350px">
-						</div>
-					</div>
-				</div>
 
-				<div class="d-flex flex-column flex-lg-row-fluid py-10">
-					<div class="d-flex flex-center flex-column flex-column-fluid">
-						<div class="w-lg-500px p-10 p-lg-15 mx-auto">
-							{{ $slot }}
-						</div>
-					</div>
-					<div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
-						<div class="d-flex flex-center fw-bold fs-6">
-							<a href="https://discord.chromecinema.hu/" class="text-muted text-hover-primary px-2" target="_blank">Discord</a>
-              				<a href="mailto:info@chromecinema.hu" class="text-muted text-hover-primary px-2" target="_blank">Email</a>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-		@include('components.scripts')
-	</body>
+    <!-- THEME SETTINGS -->
+        <style>
+        :root {
+        --color-indigo-50: 240 249 255;
+        --color-indigo-100: 224 242 254;
+        --color-indigo-200: 186 230 253;
+        --color-indigo-300: 125 211 252;
+        --color-indigo-400: 56 189 248;
+        --color-indigo-500: 14 165 233;
+        --color-indigo-600: 2 132 199;
+        --color-indigo-700: 3 105 161;
+        --color-indigo-800: 7 89 133;
+        --color-indigo-900: 12 74 110;
+        --color-voon-950: 8 8 8;
+        --color-voon-900: 10 10 10;
+        --color-voon-800: 14 14 14;
+        --color-voon-700: 18 18 18;
+        --color-voon-background: 18 18 18;
+        --color-voon-card: 14 14 14;
+        --color-neutral-900: 23 23 23;
+        --color-zinc-600: 82 82 91;
+    }
+    </style>
+
+        
+    <!-- CUSTOM CSS -->
+    <style type="text/css">
+            </style>
+
+    <!-- ROLE CSS -->
+    <style type="text/css">
+                
+                        
+                        
+                        
+                        
+                    </style>
+    </head>
+    <body x-data="body">
+        <div id="app" bis_skin_checked="1">
+            <main class="main" role="main" style="margin-bottom: 0 !important;">
+                <div class="flex flex-col lg:grid lg:grid-cols-2 min-h-screen bg-voon-bg" bis_skin_checked="1">
+                    
+                    <!-- Left Section: Image and Text -->
+                    <div class="relative hidden lg:flex items-center justify-center z-10" bis_skin_checked="1">
+                        <div class="absolute overflow-hidden rounded-l-3xl bg-cover bg-center h-full w-full" bis_skin_checked="1" style="background-image:url(https://chromecinema.hu/assets/fooldal/images/bg.png);">
+                        </div>
+                        <div class="relative z-20" bis_skin_checked="1">
+                            <img class="w-1/4 mx-auto" src="https://chromecinema.hu/assets/mozi/img/mobile-default-dark.png" alt="">
+                            <h2 class="font-bold text-3xl text-center text-white">ChromeCinema</h2>
+                            <p class="text-white/75 pl-2" style="display: block; margin-block-start: 1em; margin-block-end: 1em; margin-inline-start: 0px; margin-inline-end: 0px; width: 80%; text-align: center; margin-left: 10%;">
+                                Lépj be a mozi világába, vásárolj jegyet a neked tetsző filmre, mellé egy jó nassolnivalót, és élvezd a mozi világát.
+                            </p>
+                        </div>
+                    </div>
+    
+                    <!-- Right Section: Main Content -->
+                    <div class="relative grow flex items-center justify-center flex-col overflow-hidden px-6 py-12" bis_skin_checked="1">
+                        {{ $slot }}
+                        <div class="absolute -bottom-6 left-0 shadow-voon-bottom w-28 h-6" bis_skin_checked="1"></div>
+                        <div class="p-10 absolute top-0 right-0" bis_skin_checked="1">
+                            <a href="{{ route('fooldal') }}" class="flex items-center gap-3 bg-indigo-600/20 btn-before rounded-xl py-3 px-5 text-sm font-medium text-white hover:bg-indigo-600/40 relative hidden lg:flex items-center justify-center z-10">
+                                <i class="fas fa-arrow-left"></i>Főoldal
+                            </a>
+                        </div>
+                    </div>
+    
+                    <!-- Bottom Shadow Section -->
+                    <div class="absolute -top-6 left-[calc(50%-8rem)] shadow-voon-bottom w-64 h-6" bis_skin_checked="1"></div>
+                </div>
+            </main>
+        </div>
+    
+        @include('components.scripts')
+    </body>
+    
 </html>
