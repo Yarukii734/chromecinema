4b8ce4172557fc47107ec2ed7c343f1480cb9ba0
Yarukii734 <bojtipatrik01@gmail.com>
2025-04-09 20:54:25 +0200

    Update beallitasok.blade.php

--- resources/views/mozi/fiok/beallitasok.blade.php
+++ resources/views/mozi/fiok/beallitasok.blade.php
@@ -12,57 +12,9 @@
         <!--begin::Content-->
         <div id="kt_account_settings_profile_details" class="collapse show">
             <!--begin::Form-->
-            <form id="kt_account_profile_details_form" class="form" wire:submit.prevent="mentes" enctype="multipart/form-data">
+            <form class="form" wire:submit.prevent="mentes" enctype="multipart/form-data">
                 <!--begin::Card body-->
-                <div class="card-body border-top p-9">
-                    <!--begin::Input group-->
-                    <div class="row mb-6">
-                        <!--begin::Label-->
-                        <label for="profilkep" class="col-lg-4 col-form-label fw-semibold fs-6">Profilkép</label>
-                        <!--end::Label-->
-                        <!--begin::Col-->
-                        <div class="col-lg-8">
-                            <!--begin::Image input-->
-                            <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url('{{ asset('storage/' . $user->profilkep) }}')">
-                                <!--begin::Preview existing avatar-->
-                                <div class="image-input-wrapper w-125px h-125px" style="background-image: url('{{ asset('storage/' . $user->profilkep) }}')"></div>
-                                <!--end::Preview existing avatar-->
-                                <!--begin::Label-->
-                                <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
-                                    <i class="ki-duotone ki-pencil fs-7">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-                                    <!--begin::Inputs-->
-                                    <input type="file" wire:model="profilkep" for="profilkep" name="avatar" accept=".png, .jpg, .jpeg" />
-                                    <input type="hidden" name="avatar_remove" />
-                                    <!--end::Inputs-->
-                                </label>
-                                <!--end::Label-->
-                                <!--begin::Cancel-->
-                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Cancel avatar">
-                                    <i class="ki-duotone ki-cross fs-2">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-                                </span>
-                                <!--end::Cancel-->
-                                <!--begin::Remove-->
-                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action="remove" data-bs-toggle="tooltip" title="Remove avatar">
-                                    <i class="ki-duotone ki-cross fs-2">
-                                        <span class="path1"></span>
-                                        <span class="path2"></span>
-                                    </i>
-                                </span>
-                                <!--end::Remove-->
-                            </div>
-                            <!--end::Image input-->
-                            <!--begin::Hint-->
-                            <div class="form-text">Elfogadott képtipusok: png, jpg, jpeg.</div>
-                            <!--end::Hint-->
-                        </div>
-                        <!--end::Col-->
-                    </div>
+                <div class="card-body border-top p-9">                  
                     <!--end::Input group-->
                     <!--begin::Input group-->
                     <div class="row mb-6">
@@ -118,7 +70,26 @@
                 <!--begin::Actions-->
                 <div class="card-footer d-flex justify-content-end py-6 px-9">
                     <button type="reset" class="btn btn-light btn-active-light-primary me-2">Mégse</button>
-                    <button type="submit" class="btn btn-primary" id="kt_account_profile_details_submit">Mentés változtatása</button>
+                    <button type="submit" class="btn btn-primary"
+                style="border-radius: 4px; background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgb(79 153 183) 0%, rgb(16 197 132) 100%);"
+                wire:click="mentes"
+                wire:loading.attr="disabled"
+                wire:loading.target="mentes"
+        >
+            <!-- Normál állapot -->
+            <span wire:loading.remove wire:target="mentes">Mentés változtatása</span>
+
+            <!-- Betöltés állapot -->
+            <span wire:loading wire:target="mentes">Feldolgozás...</span>
+
+            <!-- Spinner -->
+            <span wire:loading wire:target="mentes" class="ml-2">
+                <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"></path>
+                </svg>
+            </span>
+        </button>
                 </div>
                 <!--end::Actions-->
             </form>
