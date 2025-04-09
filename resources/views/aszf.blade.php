0bd1f7ffdc05b13494a42aa102fd1db49b77854f
Yarukii734 <bojtipatrik01@gmail.com>
2025-04-09 20:46:18 +0200

    Create aszf.blade.php

---
+++ resources/views/aszf.blade.php
@@ -0,0 +1,57 @@
+<div class="section-wrapper">
+    <div class="mobile-section reaper"></div>
+
+    <div id="payment" bis_skin_checked="1">
+        <div class="wrapper" bis_skin_checked="1">
+            <h2>ÁLTALÁNOS SZERZŐDÉSI FELTÉTELEK (ÁSZF)</h2>
+
+            <p><strong>Bevezetés</strong><br>
+            A ChromeCinema.hu weboldalt (a továbbiakban: "Weboldal") üzemeltető cég a [ChromeCinema] (a továbbiakban: "Szolgáltató") által biztosított szolgáltatásokat igénybe véve a felhasználók elfogadják az alábbi Általános Szerződési Feltételek (ÁSZF) rendelkezéseit. Kérjük, figyelmesen olvassa el az alábbi feltételeket, mivel a Weboldal használata ezen feltételek elfogadását jelenti.</p>
+
+            <p><strong>1. A szolgáltatás leírása</strong><br>
+            A ChromeCinema.hu weboldal a felhasználók számára filmek és moziélmények megtekintésére, vásárlására, illetve különböző filmes tartalmakhoz való hozzáférés biztosítására szolgál. A Szolgáltató minden jogot fenntart, hogy a szolgáltatásait bármikor módosítsa, frissítse, vagy szüneteltesse.</p>
+
+            <p><strong>2. Regisztráció és fiókkezelés</strong><br>
+            A Weboldalon való regisztráció önkéntes, és a felhasználók a regisztráció során pontos és valós adatokat kötelesek megadni. A felhasználó felelőssége biztosítani, hogy a megadott adatok mindig naprakészek legyenek.</p>
+
+            <p>A felhasználó felelős az általa regisztrált fiók védelméért, valamint a fiókhoz való hozzáférés során történt minden tevékenységért.</p>
+
+            <p><strong>3. A szolgáltatás használata</strong><br>
+            A Weboldalon elérhető szolgáltatások kizárólag személyes, nem kereskedelmi célú használatra vehetők igénybe. A felhasználó nem jogosult a szolgáltatások másolására, terjesztésére, módosítására, vagy harmadik fél számára történő biztosítására.</p>
+
+            <p><strong>4. Vásárlás és fizetés</strong><br>
+            A Weboldalon elérhető szolgáltatások igénybevételéhez vásárlásra kerülhet sor, amelyet a felhasználók online, bankkártyás, vagy egyéb, a Weboldalon elérhető fizetési módokon keresztül végezhetnek el. A vásárlás során a felhasználó köteles megadni a szükséges adatokat, és elismeri, hogy a fizetés sikeres feldolgozása esetén a szolgáltatásokat igénybe vehet.</p>
+
+            <p><strong>5. Szellemi tulajdonjogok</strong><br>
+            A Weboldalon található minden tartalom (pl. szöveg, kép, logó, videó, grafika, szoftverek) a Szolgáltató vagy annak licencadóinak szellemi tulajdonát képezi, és a szerzői jog védelme alá esik. A felhasználók a Weboldalon elérhető tartalmakat kizárólag a személyes, nem kereskedelmi célú használatra jogosultak felhasználni.</p>
+
+            <p><strong>6. A szolgáltatás szüneteltetése és megszüntetése</strong><br>
+            A Szolgáltató fenntartja a jogot, hogy bármikor szüneteltesse, módosítsa, vagy megszüntesse a Weboldalon elérhető szolgáltatásokat. A felhasználók a Weboldalon történő regisztrációval tudomásul veszik, hogy a Szolgáltató nem vállal felelősséget a szolgáltatások esetleges szüneteltetéséből, megszüntetéséből eredő károkért.</p>
+
+            <p><strong>7. Adatvédelem</strong><br>
+            A Weboldalon végzett tevékenységek során a Szolgáltató az adatkezelésről és adatvédelmi irányelvekről szóló tájékoztatót biztosít. A felhasználó hozzájárulását adja személyes adatainak kezeléséhez a regisztrációval és a szolgáltatások igénybevételével.</p>
+
+            <p><strong>8. Felelősségkorlátozás</strong><br>
+            A Szolgáltató nem vállal felelősséget a Weboldalon elérhető tartalmak pontosságáért, megbízhatóságáért, és nem garantálja, hogy a Weboldal folyamatosan elérhető lesz, vagy hogy a felhasználók számára hibamentes élményt biztosít. A Weboldalon keresztül elérhető tartalmak harmadik féltől származhatnak, így a Szolgáltató nem vállal felelősséget azok minőségéért, hitelességéért.</p>
+
+            <p><strong>9. Panaszkezelés</strong><br>
+            A felhasználóknak lehetőségük van a Szolgáltatóval való kapcsolattartásra a Weboldalon elérhető kapcsolati űrlapon vagy ügyfélszolgálati elérhetőségeken keresztül. A Szolgáltató a beérkező panaszokat ésszerű határidőn belül megválaszolja.</p>
+
+            <p><strong>10. Jogviták rendezése</strong><br>
+            A jelen ÁSZF-ből eredő jogviták esetén a felek a magyar jog szerint járnak el, és a jogvita rendezésére a Szolgáltató székhelye szerinti bíróságok kizárólagos illetékességét fogadják el.</p>
+
+            <p><strong>11. Záró rendelkezések</strong><br>
+            A Szolgáltató fenntartja a jogot, hogy a jelen ÁSZF-et bármikor módosítsa. A módosított ÁSZF hatályba lépését követően a Weboldal használata a módosított feltételek elfogadását jelenti. A módosított ÁSZF a Weboldalon való közzététellel válik érvényessé.</p>
+
+            <p>A ChromeCinema.hu weboldalt üzemelteti:<br>
+                ChromeCinema Kft.<br>
+                4700 Mátészalka, Kölcsey utca 12.<br>
+                info@chromecinema.hu<br>
+                +36 30 8770689</p>            
+
+            <p><span class="copyright">Copyright <span class="year">2021 </span><br> ChromeCinema.hu © Minden jog fenntartva!</span></p>
+
+        </div>
+        <br>
+    </div>
+</div>
