39341efbab4c031eb66f82e456c2410e573e806a
Yarukii734 <bojtipatrik01@gmail.com>
2025-04-09 20:50:14 +0200

    Update sidebar.blade.php

--- resources/views/components/mozi/sidebar.blade.php
+++ resources/views/components/mozi/sidebar.blade.php
@@ -21,9 +21,9 @@
         <!--begin::Menu wrapper-->
         <div id="kt_app_sidebar_menu_wrapper" class="app-sidebar-wrapper">
             <!--begin::Scroll wrapper-->
-            <div id="kt_app_sidebar_menu_scroll" class="scroll-y my-5 mx-3" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true">
+            <div id="kt_app_sidebar_menu_scroll" class="scroll-y my-5 mx-3" data-kt-scroll="true" data-kt-scroll-activate="true" data-kt-scroll-height="auto" data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer" data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" data-kt-scroll-save-state="true" bis_skin_checked="1" style="height: 766px;">
                 <!--begin::Menu-->
-                <div class="menu menu-column menu-rounded menu-sub-indention fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false">
+                <div class="menu menu-column menu-rounded menu-sub-indention fw-semibold fs-6" id="#kt_app_sidebar_menu" data-kt-menu="true" data-kt-menu-expand="false" bis_skin_checked="1">
                     <!--begin:Menu item-->
                     <div class="menu-item">
                         <!--begin:Menu link-->
@@ -83,31 +83,6 @@
                     </div>
                     <!--end:Menu item-->
                     <!--begin:Menu item-->
-                    <div class="menu-item pt-5">
-                        <!--begin:Menu content-->
-                        <div class="menu-content">
-                            <span class="menu-heading fw-bold text-uppercase fs-7">Egyéb</span>
-                        </div>
-                        <!--end:Menu content-->
-                    </div>
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="../../demo1/dist/apps/calendar.html">
-                            <span class="menu-icon">
-                                <i class="ki-duotone ki-calendar-8 fs-2">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                    <span class="path3"></span>
-                                    <span class="path4"></span>
-                                    <span class="path5"></span>
-                                    <span class="path6"></span>
-                                </i>
-                            </span>
-                            <span class="menu-title">Calendar</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
                     <!--end:Menu item-->
                     <!--begin:Menu item-->
                     <div class="menu-item pt-5">
@@ -132,6 +107,7 @@
                         </a>
                         <!--end:Menu link-->
                     </div>
+
                     <!--end:Menu item-->
                     <!--begin:Menu item-->
                     <div class="menu-item pt-5">
@@ -145,49 +121,19 @@
                     <!--begin:Menu item-->
                     <div class="menu-item">
                         <!--begin:Menu link-->
-                        <a class="menu-link" href="{{ route('mozi.fooldal') }}" target="_blank">
+                        <a class="menu-link {{ Request::is('rolunk') ? 'active' : '' }}" href="{{ route('mozi.rolunk') }}">
                             <span class="menu-icon">
                                 <i class="ki-duotone ki-rocket fs-2">
                                     <span class="path1"></span>
                                     <span class="path2"></span>
                                 </i>
                             </span>
-                            <span class="menu-title">Components</span>
+                            <span class="menu-title">Rólunk</span>
                         </a>
                         <!--end:Menu link-->
                     </div>
                     <!--end:Menu item-->
                     <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="{{ route('mozi.fooldal') }}" target="_blank">
-                            <span class="menu-icon">
-                                <i class="ki-duotone ki-abstract-26 fs-2">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                </i>
-                            </span>
-                            <span class="menu-title">Documentation</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
-                    <!--end:Menu item-->
-                    <!--begin:Menu item-->
-                    <div class="menu-item">
-                        <!--begin:Menu link-->
-                        <a class="menu-link" href="{{ route('mozi.fooldal') }}" target="_blank">
-                            <span class="menu-icon">
-                                <i class="ki-duotone ki-code fs-2">
-                                    <span class="path1"></span>
-                                    <span class="path2"></span>
-                                    <span class="path3"></span>
-                                    <span class="path4"></span>
-                                </i>
-                            </span>
-                            <span class="menu-title">Changelog v8.2.0</span>
-                        </a>
-                        <!--end:Menu link-->
-                    </div>
                     <!--end:Menu item-->
                 </div>
                 <!--end::Menu-->
