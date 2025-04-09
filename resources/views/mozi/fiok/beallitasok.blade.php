a70a9e94b760a69fbe204f19db2171767b872532
Yarukii734 <bojtipatrik01@gmail.com>
2025-04-09 20:54:32 +0200

    Update beallitasok.blade.php

--- resources/views/mozi/fiok/beallitasok.blade.php
+++ resources/views/mozi/fiok/beallitasok.blade.php
@@ -14,7 +14,39 @@
             <!--begin::Form-->
             <form class="form" wire:submit.prevent="mentes" enctype="multipart/form-data">
                 <!--begin::Card body-->
-                <div class="card-body border-top p-9">                  
+                <div class="card-body border-top p-9">
+                    <!--Profilkep
+                    <div class="row mb-6">
+                        <label class="col-lg-4 col-form-label fw-semibold fs-6">Profilkép</label>
+                        <div class="col-lg-8">
+                            <div class="image-input image-input-outline" data-kt-image-input="true" style="background-image: url('{{ asset('default-avatar.jpg') }}')">
+                                
+                                <div class="image-input-wrapper w-125px h-125px" 
+                                    style="background-image: url('{{ asset('default-avatar.jpg') }}')">
+                                </div>
+                    
+                                <label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" 
+                                    data-kt-image-input-action="change" data-bs-toggle="tooltip" title="Change avatar">
+                                    <i class="ki-duotone ki-pencil fs-7"></i>
+                                    <input type="file" wire:model="profilkep" accept=".png, .jpg, .jpeg">
+
+                                </label>
+                    
+                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" 
+                                    data-kt-image-input-action="cancel" data-bs-toggle="tooltip" title="Cancel avatar">
+                                    <i class="ki-duotone ki-cross fs-2"></i>
+                                </span>
+                    
+                                <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" 
+                                    data-bs-toggle="tooltip" title="Remove avatar">
+                                    <i class="ki-duotone ki-cross fs-2"></i>
+                                </span>
+                            </div>
+                    
+                            <div class="form-text">Elfogadott képtípusok: png, jpg, jpeg.</div>
+                        </div>
+                    </div>-->
+                    
                     <!--end::Input group-->
                     <!--begin::Input group-->
                     <div class="row mb-6">
