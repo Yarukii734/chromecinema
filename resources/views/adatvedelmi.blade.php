ac83a4d6b52ecb4e4f79669792be61a5074dfb66
Yarukii734 <bojtipatrik01@gmail.com>
2025-04-09 20:46:36 +0200

    Create adatvedelmi.blade.php

---
+++ resources/views/adatvedelmi.blade.php
@@ -0,0 +1,92 @@
+<div class="section-wrapper">
+    <div class="mobile-section reaper"></div>
+
+    <div id="payment" bis_skin_checked="1">
+        <div class="wrapper" bis_skin_checked="1">
+            <h2>ADATVÉDELMI TÁJÉKOZTATÓ</h2>
+    
+            <p><strong>Bevezetés</strong><br>
+            A ChromeCinema.hu weboldal (a továbbiakban: "Weboldal") működtetője, a ChromeCinema Kft. (székhely: 4700 Mátészalka, Kölcsey utca 12., e-mail: info@chromecinema.hu, telefonszám: +36 30 8770689) kiemelten fontosnak tartja a felhasználói személyes adatainak védelmét. Az alábbiakban tájékoztatást adunk arról, hogyan kezeljük a felhasználók személyes adatait.</p>
+    
+            <p><strong>1. Az adatkezelés célja</strong><br>
+            A Weboldal használata során gyűjtött személyes adatokat az alábbi célokból kezeljük:
+            <br>- Regisztráció és fiókkezelés,
+            <br>- Mozijegy-vásárlás és foglalás,
+            <br>- Hűségprogramok és kedvezmények biztosítása,
+            <br>- Szolgáltatások nyújtása és fejlesztése,
+            <br>- Online fizetések lebonyolítása,
+            <br>- Hírlevelek és promóciós anyagok küldése (hozzájárulás esetén),
+            <br>- Panaszkezelés és ügyfélszolgálati kapcsolattartás,
+            <br>- Filmelőzetesek és egyedi ajánlatok küldése,
+            <br>- Jogi kötelezettségek teljesítése.</p>
+    
+            <p><strong>2. Az adatkezelés jogalapja</strong><br>
+            A személyes adatok kezelésének jogalapja lehet:
+            <br>- Az érintett hozzájárulása,
+            <br>- Szerződés teljesítése,
+            <br>- Jogi kötelezettség teljesítése,
+            <br>- Jogos érdek (pl. visszaélések megelőzése, ügyfélélmény javítása).</p>
+    
+            <p><strong>3. A kezelt adatok köre</strong><br>
+            A Weboldalon gyűjtött személyes adatok a következők lehetnek:
+            <br>- Név,
+            <br>- E-mail cím,
+            <br>- Felhasználónév,
+            <br>- Számlázási és szállítási cím,
+            <br>- Fizetési információk (pl. tranzakciós adatok),
+            <br>- IP-cím, böngészési adatok,
+            <br>- Mozijegy-vásárlási és foglalási előzmények,
+            <br>- Hűségprogramhoz kapcsolódó adatok,
+            <br>- Kedvenc filmek és műfajok,
+            <br>- Részvétel promóciókban és nyereményjátékokban.</p>
+    
+            <p><strong>4. Az adatok tárolásának időtartama</strong><br>
+            A személyes adatokat addig őrizzük meg, ameddig az a fenti célok teljesítéséhez szükséges, illetve a vonatkozó jogszabályok előírásainak megfelelően.</p>
+    
+            <p><strong>5. Az adatok megosztása és továbbítása</strong><br>
+            A Weboldal csak az alábbi esetekben osztja meg a személyes adatokat harmadik felekkel:
+            <br>- Számlázási és fizetési szolgáltatókkal,
+            <br>- Jogi kötelezettség teljesítése érdekében hatóságokkal,
+            <br>- Weboldal üzemeltetési és marketing szolgáltatókkal,
+            <br>- Filmforgalmazó partnerekkel (pl. elővételes jegyvásárlások esetén),
+            <br>- Hűségprogram és promóciós kampányok lebonyolítóival.</p>
+    
+            <p><strong>6. Felhasználói jogok</strong><br>
+            A felhasználóknak joguk van:
+            <br>- Hozzáférni a róluk tárolt személyes adatokhoz,
+            <br>- Kérni az adatok helyesbítését vagy törlését,
+            <br>- Tiltakozni az adatkezelés ellen,
+            <br>- Kérni az adatok hordozhatóságát,
+            <br>- Panaszt tenni az illetékes adatvédelmi hatóságnál.</p>
+    
+            <p><strong>7. Adatbiztonság</strong><br>
+            A ChromeCinema Kft. különös figyelmet fordít az adatbiztonságra, és megteszi a szükséges technikai és szervezeti intézkedéseket a személyes adatok védelme érdekében. Az adatokhoz való hozzáférést kizárólag jogosultsággal rendelkező munkatársak és partnerek kapják meg.</p>
+    
+            <p><strong>8. Cookie-k (sütik) kezelése</strong><br>
+            A Weboldal cookie-kat használ a felhasználói élmény javítására, beleértve a filmelőzetesek és ajánlatok testreszabását. A felhasználók bármikor módosíthatják cookie-beállításaikat a böngészőben.</p>
+    
+            <p><strong>9. Kapcsolat</strong><br>
+            Amennyiben bármilyen kérdése van az adatkezeléssel kapcsolatban, kérjük, lépjen kapcsolatba velünk az alábbi elérhetőségeken:<br>
+            - E-mail: info@chromecinema.hu<br>
+            - Telefonszám: +36 30 8770689<br>
+            - Székhely: 4700 Mátészalka, Kölcsey utca 12.</p>
+    
+            <p><strong>10. Az Adatvédelmi Tájékoztató módosítása</strong><br>
+            A ChromeCinema Kft. fenntartja a jogot, hogy az Adatvédelmi Tájékoztatót bármikor módosítsa. Az aktuális változat mindig elérhető a Weboldalon.</p>
+    
+            <p>A ChromeCinema.hu weboldalt üzemelteti:<br>
+                ChromeCinema Kft.<br>
+                4700 Mátészalka, Kölcsey utca 12.<br>
+                info@chromecinema.hu<br>
+                +36 30 8770689</p>
+    
+            <p><span class="copyright">Copyright <span class="year">2021 </span><br> ChromeCinema.hu © Minden jog fenntartva!</span></p>
+    
+        </div>
+        <br>
+    </div>
+        <br>
+    </div>
+        
+    </div>
+</div>
